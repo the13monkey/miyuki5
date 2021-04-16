8970a061ecfd1bbc2f51b1a176d9914305981e2d
Dinah Chen <dinah.chen.ccy@gmail.com>
2021-04-16 16:23:25 -0700

    templates done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $("#lyric-book").load("flip.html")
+    $(".content-list-item").click(function(){
+        var url = $(this).data("url")
+        window.location.replace(url)
+    });
 })
